6fd025cccc5a066e7189894d9bc27a8d03cb91f6
Spectrum-Edge <50417942+Spectrum-Edge@users.noreply.github.com>
2022-05-14 22:39:29 +0100

    Beginning

--- ccStartup.lua
+++ ccStartup.lua
@@ -1,5 +1,29 @@
-print("El goblino")
 
+-- Peripheral setup
+local p_monitor = "right"
 
-local monitor = peripheral.wrap("right")
-monitor.write("Hi Sky :3")
+-- Constants
+local greetings = {
+    "Hi Spectrum!!",
+    "Hello Spectrum!",
+    "Beep boop!"
+}
+
+-- Setup monitor initially
+local function SetupMonitor()
+    if peripheral.isPresent(p_monitor) then
+        -- Wrap monitor
+        local monitor = peripheral.wrap(p_monitor)
+
+        -- Set monitor color scheme
+
+        -- Clear and set cursor position
+        monitor.clear()
+        monitor.setCursorPos(1,1)
+        
+        -- Display greeting
+        print(math.random(1,#greetings))
+    end
+end
+
+SetupMonitor()
