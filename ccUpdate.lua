d3b1eb755b124eb5c9b37ac6dd4e09f3ff282682
Spectrum-Edge <50417942+Spectrum-Edge@users.noreply.github.com>
2022-05-15 10:11:04 +0100

    Update ccUpdate.lua

--- ccUpdate.lua
+++ ccUpdate.lua
@@ -9,5 +9,6 @@ local function updateFile(fileName, url)
 end
 
 -- Get github version of update (write mode w overwrites any existing content)
-updateFile("startup", "https://raw.githubusercontent.com/Spectrum-Edge/ccSpecOS/main/ccUpdate.lua")
-updateFile("update", "https://raw.githubusercontent.com/Spectrum-Edge/ccSpecOS/main/ccStartup.lua")
+updateFile("startup", "https://raw.githubusercontent.com/Spectrum-Edge/ccSpecOS/main/ccStartup.lua")
+updateFile("update", "https://raw.githubusercontent.com/Spectrum-Edge/ccSpecOS/main/ccUpdate.lua")
+updateFile("shutdown", "https://raw.githubusercontent.com/Spectrum-Edge/ccSpecOS/main/ccShutdown.lua")
